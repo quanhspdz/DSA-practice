1b0bff813d09f39722cc5784d6c1ea9257e0e53c
quanhspdz <pc.quanhdz@gmail.com>
2021-10-21 07:37:19 +0700

    add week-6 again

--- week-6/ex1.cpp
+++ week-6/ex1.cpp
@@ -1,1 +1,38 @@
-unsupported reparse point
+#include <iostream>
+#include <map>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int command, id;
+    string name;
+    map<int, string> m;
+    while (n > 0) {
+        n--;
+        cin >> command;
+        switch (command) {
+            case 1: {
+                cin >> id >> name;
+                m[id] = name;
+                break;
+            }
+            case 2: {
+                cin >> id;
+                m.erase(id);
+                break;
+            }
+            case 3: {
+                cin >> id;
+                if (m.find(id) == m.end()) {
+                    cout << "n/a" << endl;
+                } else {
+                    cout << m[id] << endl;
+                }
+            }
+        }
+    }
+
+    return 0;
+}
--- week-6/ex2.cpp
+++ week-6/ex2.cpp
@@ -1,1 +1,22 @@
-unsupported reparse point
+#include <iostream>
+#include <map>
+using namespace std;
+
+int main() {
+    int n, num;
+    cin >> n;
+    map<int, int> count;
+
+    while (n > 0) {
+        n--;
+        cin >> num;
+        if (count.find(num) == count.end()) {
+            count[num] = 1;
+        } else {
+            count[num]++;
+        }
+        cout << count[num] << endl;
+    }
+
+    return 0;
+}
