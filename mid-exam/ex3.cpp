3ba589ba548f7ef9f882fc456ac2bfbb6260dce6
quanhspdz <pc.quanhdz@gmail.com>
2021-10-27 07:42:17 +0700

    add ex3.cpp

--- mid-exam/ex3.cpp
+++ mid-exam/ex3.cpp
@@ -2,6 +2,17 @@
 #include <algorithm>
 using namespace std;
 
+double medianOfArray(int arr[], int n) {
+    sort(arr, arr + n);
+    int mid = n / 2;
+    double result;
+    if (n % 2 == 0) {
+        return (double) (arr[mid] + arr[mid - 1]) / 2;
+    } else {
+        return (double) arr[mid];
+    }
+}
+
 int main() {
     int n;
     cin >> n;
@@ -10,17 +21,9 @@ int main() {
         cin >> arr[i];
     }
 
-    sort(arr, arr + n);
-    int mid = n / 2;
-    double result;
-    if (n % 2 == 0) {
-        result = (double) (arr[mid] + arr[mid - 1]) / 2;
-    } else {
-        result = (double) arr[mid];
+    for (int i = 0; i < n; i++) {
+        printf("%0.1lf\n", medianOfArray(arr, i+ 1));
     }
-
-    printf("%0.1lf\n", result);
-
 
     return 0;
 }
