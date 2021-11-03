02a3561bb32d809e117283c1f369e02e9b9d2c95
quanhspdz <pc.quanhdz@gmail.com>
2021-11-03 08:56:28 +0700

    add something

--- mid-exam/ex3.cpp
+++ mid-exam/ex3.cpp
@@ -22,7 +22,7 @@ int main() {
     }
 
     for (int i = 0; i < n; i++) {
-        printf("%0.1lf\n", medianOfArray(arr, i+ 1));
+        printf("%0.1lf\n", medianOfArray(arr, i + 1));
     }
 
     return 0;
