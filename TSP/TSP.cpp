0f220fc0e5b89fc6896e5d997e9ae7f19415f305
quanhspdz <pc.quanhdz@gmail.com>
2021-11-20 20:13:48 +0700

    add function to show the TSP path

--- TSP/TSP.cpp
+++ TSP/TSP.cpp
@@ -8,20 +8,20 @@ int dist[1000][1000];
 //mang sau de luu cac khoang cach da duoc tinh truoc (quy hoach dong):
 int tempDist[1000][1000];
 //string luu tien trinh di:
-string minRoadMap = "";
-
+int path[1000][1000];
+int start;
 int VISITED_ALL = (1 << n) - 1;
 
-int TSP(int mask, int pos, string roadMap) {
+int TSP(int mask, int pos) {
     //neu da tham het cac dinh thi return:
     if (mask == VISITED_ALL) {
         return dist[pos][0];
     }
 
     //neu khoang cach da duoc tinh truoc thi tai su dung:
-    // if (tempDist[mask][pos] != -1) {
-    //     return tempDist[mask][pos];
-    // }
+    if (tempDist[mask][pos] != -1) {
+        return tempDist[mask][pos];
+    }
 
     int minWeight = INT_MAX;
     int minPos = INT_MAX;
@@ -29,17 +29,34 @@ int TSP(int mask, int pos, string roadMap) {
     for (int i = 0; i < n; i++) {
         //neu chua dinh chua duoc tham thi:
         if ((mask & (1 << i)) == 0) {
-            int newWeight = dist[pos][i] + TSP(mask | (1 << i), i, roadMap);
-            minWeight = min(minWeight, newWeight);
+            int newWeight = dist[pos][i] + TSP(mask | (1 << i), i);
+            if (minWeight > newWeight) {
+                minWeight = newWeight;
+                minPos = i;
+            }
         }
     }
 
+    path[mask][pos] = minPos;
+
     return tempDist[mask][pos] = minWeight;
+}
+void showPath_TSP(int mask, int pos) {
+    if (pos == start) {
+        cout << "(" << start << ") -> ";
+    }
+    if (path[mask][pos] != -1) {
+        int minPos = path[mask][pos];
+        cout << "(" << minPos << ") " << "-> ";
+        showPath_TSP(mask | (1 << minPos), minPos);
+    } else {
+        cout << "return (" << start << ") ";
+    }
 }
 
 int main() {
     //du lieu vao:
-    cin >> n;
+    cin >> n >> start;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             cin >> dist[i][j];
@@ -53,8 +70,16 @@ int main() {
             tempDist[i][j] = -1;
         }
     }
+    for (int i = 0; i < (1 << n); i++) {
+        for (int j = 0; j < n; j++) {
+            path[i][j] = -1;
+        }
+    }
 
-    cout << TSP(1, 0, "0") << endl;
+    cout << "The distance: " << TSP(1, start) << endl;
+    cout << "The path: ";
+    showPath_TSP(1, start);
 
+    cout << endl;
     return 0;
 }
